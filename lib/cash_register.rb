948f2e83c7f8fa8f1df58a254e557db616c37e1a
Vivica <Ma>
2020-03-12 23:35:52 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,71 +1,39 @@
-# class CashRegister 
-#     attr_accessor :total, :discount, :items
-#     def initialize(discount=0)
-#         @total = 0
-#         @discount = discount
-#         @items = []
-#     end 
+class CashRegister 
+    attr_accessor :total, :discount, :items, :last_transaction
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end 
 
-#     def add_item(title, price, quantity=1)
-#         self.total += price * quantity
-#         quantity.times do 
-#             items << title 
-#         end 
-#         self.last_transaction = price * quantity
-#     end
+    def add_item(title, price, quantity=1)
+        self.total += price * quantity
+        quantity.times do 
+            items << title 
+        end 
+        self.last_transaction = price * quantity
+    end
 
-#     def apply_discount
+    def apply_discount
         
-#         if discount != 0
-#             self.total = (total * ((100.0 - discount.to_f)/100.0)).to_i 
-#             "After the discount, the total comes to $#{self.total}."
-#         else 
-#             "There is no discount to apply."
-#         end 
-#     end 
+        if discount != 0
+            self.total = (total * ((100.0 - discount.to_f)/100.0)).to_i 
+            "After the discount, the total comes to $#{self.total}."
+        else 
+            "There is no discount to apply."
+        end 
+    end 
 
-#     # def items
-#     #     self.add_item.select do |item|
-#     #         items << item.title 
-#     #     end     
-#     # end
+    # def items
+    #     self.add_item.select do |item|
+    #         items << item.title 
+    #     end     
+    # end
 
-#     def void_last_transaction
-#         self.total = self.total - self.last_transaction
-#     end
-
-
-# end 
-
-
-class CashRegister
-
-  attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
+    def void_last_transaction
+        self.total = self.total - self.last_transaction
     end
-    self.last_transaction = amount * quantity
-  end
 
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
-    end
-  end
 
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
-end
+end 
+
